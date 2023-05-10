de19b5753e54f1f09c06148dd14cf2a41794a211
Kristina Orlichenko <chrisorlichenko@gmail.com>
2023-05-10 15:03:47 +0000

    Deployment part

--- beautybliss/settings.py
+++ beautybliss/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-#kogl6$lg-!0hv583d(k$%bwknc61=j#e^m(a*ui2kpcm4u0=d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['beautybliss.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,13 +125,18 @@ WSGI_APPLICATION = 'beautybliss.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
