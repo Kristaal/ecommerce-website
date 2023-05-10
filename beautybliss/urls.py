7202b2b84889c98d0829ceb2828faba48e9464a5
Kristina Orlichenko <chrisorlichenko@gmail.com>
2023-05-10 14:07:16 +0000

    Add 404 page

--- beautybliss/urls.py
+++ beautybliss/urls.py
@@ -1,7 +1,9 @@
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,3 +17,5 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'beautybliss.views.handler404'
